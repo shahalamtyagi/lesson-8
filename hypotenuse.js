c8a5971868ec0237d28c0b2b0f79e3fe7ac9f424
shahalamtyagi <118383617+shahalamtyagi@users.noreply.github.com>
2023-02-26 19:15:45 +0530

    add navigation bar

--- hypotenuse.js
+++ hypotenuse.js
@@ -2,15 +2,15 @@ const sideInput = document.querySelectorAll(".side-input")
 const hypotenuseBtn = document.querySelector("#hypotenuse-btn")
 const outputBox = document.querySelector("#output")
 
-function calculateSumOfSquare (a,b){
-    const sumOfSquare = a*a + b*b;
-    return sumOfSquare;
+function calculateSumOfSquare(a,b){
+    const sumOfSquares = a*a + b*b;
+    return sumOfSquares;
 }
 
 function calculateHypotenuse(){
-    const sumOfSquare = calculateSumOfSquare(Number(sides[0].value),Number(sides[1].value));
-const lengthOfHypotenuse = Math.sqrt (sumOfSquare)
+    const sumOfSquares = calculateSumOfSquare(Number(sideInput[0].value),Number(sideInput[1].value));
+const lengthOfHypotenuse = Math.sqrt(sumOfSquares);
 outputBox.innerText = "The length of hypotenuse " + lengthOfHypotenuse
 }
 
-hypotenuseBtn.addEventListener("click", calculateSumOfSquare)
+ hypotenuseBtn.addEventListener("click", calculateHypotenuse);
--- quiz.js
+++ quiz.js
@@ -1,19 +1,40 @@
-const quizForm = document.querySelector("#quiz-form");
+const quizForm = document.querySelector(".quiz-form");
 const submitBtn = document.querySelector("#submit-Btn");
 const outputBox = document.querySelector("#output");
 
-const correctAnswers = ["90", "right angled"];
+
+const correctAnswers = ["90°", "right angled"];
+
 function calculateScore() {
   let score = 0;
   let index = 0;
-  const data = new FormData(quizForm);
-  for (let value of data.value()) {
-    if (value === correctAnswers[index]) {
+  const formResults = new FormData(quizForm);
+  for(let value of formResults.values()){
+    if(value === correctAnswers[index]){
       score = score + 1;
     }
     index = index + 1;
   }
-  console.log(score);
-//   outputBox.innerText = "Your score is " + score;
+  //   console.log(score);
+  outputBox.innerText = "Your score is " + score;
 }
 submitBtn.addEventListener("click", calculateScore);
+
+
+
+// const correctAnswers = ["90°", "right angled"];
+
+// function calculateScore() {
+//   let score = 0;
+//   let index = 0;
+//   const formResults = new FormData(quizForm);
+//   for(let value of formResults.values()){
+//     if(value === correctAnswers[index]){
+//       score = score + 1;
+//     }
+//     index = index + 1;
+//   }
+//   //   console.log(score);
+//   outputBox.innerText = "Your score is " + score;
+// }
+// submitBtn.addEventListener("click", calculateScore);
